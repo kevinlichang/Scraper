f5855d1a5d30bb072a59acf10189b01bdad9b7bb
Kchar <38297595+kevinlichang@users.noreplay.github.com>
2018-11-08 19:36:48 -0500

    updated functions

--- public/app.js
+++ public/app.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
     data.forEach((result) => {
       let newCard = $("<div>").addClass("card m-3");
-      let newTitle = $("<div>").addClass("card-header").text(result.title);
+      let newTitle = $("<div>").addClass("card-header").html(`<h5><strong>Title: </strong>${result.title}</h5>`);
       let newBlurb = $("<div>").addClass("card-body").text(result.blurb);
       let articleLink = $("<a>").addClass("btn btn-outline-info m-1").attr({"href": result.link, "target": "_blank"}).text(`Click Here for Article Page`);
       let comments = $("<button>").addClass("btn btn-outline-info m-1 comment-btn").attr("data-id", result._id).text("Comments");
@@ -64,18 +64,21 @@ $(document).ready(function() {
         console.log(data);
         var cardBody = $("<div>").addClass("card-body");
 
-        data.notes.forEach((result) => {
-          let commentCard = $("<div>").addClass("card");
-          var commentName = $("<h5>").addClass("card-title").text(result.name);
-          var commentBody = $("<p>").addClass("card-text").text(result.body);
-          var deleteComm = $("<button>").addClass("delete-comment btn btn-outline-info m-1").attr("data-id", result._id).text("Delete Comment");
+        if (data.notes.length === 0) {
+          cardBody.append(`<h5>No Comments Yet</h5>`)
+        } else {
+          data.notes.forEach((result) => {
+            let commentCard = $("<div>").addClass("card");
+            var commentName = $("<h5>").addClass("card-title").text(result.name).append("<hr>");
+            var commentBody = $("<p>").addClass("card-text").text(result.body);
+            var deleteComm = $("<button>").addClass("delete-comment btn btn-outline-info m-1").attr("data-id", result._id).text("Delete Comment");
 
-          commentCard.append(commentName, commentBody, deleteComm).appendTo(cardBody);
-          
-        })
+            commentCard.append(commentName, commentBody, deleteComm).appendTo(cardBody); 
+          })
+        }
 
         let newCard = $("<div>").addClass("card m-3");
-        let title = $("<div>").addClass("card-header").text(data.title);
+        let title = $("<div>").addClass("card-header").html(`<h5>${data.title}</h5>`);
 
         newCard.append(title).append(cardBody);
  
@@ -84,7 +87,7 @@ $(document).ready(function() {
           .append(newCard)
           .append('<input type="text" class="form-control" id="nameinput">')
           .append("<textarea class='form-control' id='bodyinput' name='body'></textarea>")
-          .append(`<button data-id="${data._id}" id='post-comment'>Post Comment</button>`);
+          .append(`<button data-id="${data._id}" class="btn btn-outline-success" id='post-comment'>Post Comment</button>`);
 
         
       })
